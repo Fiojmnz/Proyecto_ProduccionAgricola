ad2bb4b4314b66c2db899ae649e08dfc9c9eca20
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-27 21:31:25 -0600

    Modificacion del modelo Usuario

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -9,5 +9,50 @@ package Modelo;
  * @author AsusVivobook
  */
 public class Usuario {
-    
+    private Long id;
+    private String username;
+    private String passwordHash;
+    private Rol rol;
+    private boolean activo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
 }
