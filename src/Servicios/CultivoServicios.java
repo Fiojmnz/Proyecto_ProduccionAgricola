11f61b9ba90274ba93c85d42bc4883483a8287f3
Gipsy Aguirre L <giaguirrelo@est.utn.ac.cr>
2025-11-28 17:46:00 -0600

    Se crea el servicio Cultivo

---
+++ src/Servicios/CultivoServicios.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servicios;
+
+import DAO.CultivoDAO;
+import Mapper.CultivoMapper;
+import Modelo.Cultivo;
+import Modelo.CultivoDTO;
+import Validaciones.CultivoValidador;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author gipsy
+ */
+public class CultivoServicios {
+
+    private final CultivoDAO dao;
+    private final CultivoValidador validator = new CultivoValidador();
+
+    public CultivoServicios(CultivoDAO dao) 
+    {   this.dao = dao; }
+
+    public CultivoDTO crear(CultivoDTO dto) {
+        validator.validarCrear(dto);
+        Cultivo c = CultivoMapper.toEntity(dto);
+        dao.agregar(c);
+        return CultivoMapper.toDTO(c);
+    }
+
+    public List<CultivoDTO> listar(String filtroNombre) {
+        return dao.listar(filtroNombre).stream().map(CultivoMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public boolean actualizar(CultivoDTO dto) {
+        validator.validarCrear(dto);
+        return dao.actualizar(CultivoMapper.toEntity(dto));
+    }
+
+    public boolean eliminar(int id) {
+        return dao.eliminar(id);
+    }
+}
+    
+
+
+
