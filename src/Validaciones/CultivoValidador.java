654f33d422f0448aa4ec2992319fd2e43a2e8ccf
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-29 00:14:37 -0600

    Se modifica la validacion del cultivo

--- src/Validaciones/CultivoValidador.java
+++ src/Validaciones/CultivoValidador.java
@@ -4,7 +4,7 @@
  */
 package Validaciones;
 
-import Excepciones.ValidacionException;
+import Excepciones.ValidacionExcepcion;
 import Modelo.CultivoDTO;
 
 /**
@@ -13,17 +13,17 @@ import Modelo.CultivoDTO;
  */
 public class CultivoValidador {
      public void validarCrear(CultivoDTO dto) {
-        if (dto == null) throw new ValidacionException("Datos de cultivo vacíos");
+        if (dto == null) throw new ValidacionExcepcion("Datos de cultivo vacíos");
         if (dto.getNombre() == null || dto.getNombre().isBlank())
-            throw new ValidacionException("El nombre del cultivo es obligatorio");
+            throw new ValidacionExcepcion("El nombre del cultivo es obligatorio");
         if (dto.getTipo() == null)
-    throw new ValidacionException("El tipo de cultivo es obligatorio");
+    throw new ValidacionExcepcion("El tipo de cultivo es obligatorio");
         if (dto.getAreaSembrada() <= 0)
-            throw new ValidacionException("El área sembrada debe ser mayor a 0");
+            throw new ValidacionExcepcion("El área sembrada debe ser mayor a 0");
          if (dto.getEstadoCrecimiento() == null)
-    throw new ValidacionException("El estado de crecimiento es obligatorio");
+    throw new ValidacionExcepcion("El estado de crecimiento es obligatorio");
         if (dto.getFechaSiembra() == null)
-            throw new ValidacionException("La fecha de siembra es obligatoria");
+            throw new ValidacionExcepcion("La fecha de siembra es obligatoria");
     }
 }
 
