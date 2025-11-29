94134869fd92cc39149152ce2ca85b745bd0e10b
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-29 00:25:33 -0600

    Se crea nueva clase
    Se crea la clase para guardar o encriptar contraseñas de los usuarios

---
+++ src/Validaciones/EncriptadorContraseña.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Validaciones;
+
+import java.security.MessageDigest;
+
+/**
+ *
+ * @author AsusVivobook
+ */
+public class EncriptadorContraseña {
+    public static String hash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al encriptar la contraseña", e);
+        }
+    }
+}
