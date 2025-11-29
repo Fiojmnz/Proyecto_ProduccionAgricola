a82c40ffa4f5e6c008e05ca7acd2fb87187ef7f1
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-29 00:11:26 -0600

    Se crea clase
    Se crea la clase del UsuarioDTO

---
+++ src/Modelo/UsuarioDTO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import Enum.Rol;
+
+/**
+ *
+ * @author AsusVivobook
+ */
+public class UsuarioDTO {
+    private String username;
+    private String password;
+    private Rol rol;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+}
