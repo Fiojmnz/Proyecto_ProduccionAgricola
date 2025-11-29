d4ba52a07b080ed68ce5ed830fb510928bde7533
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-29 01:07:09 -0600

    Creacion
    Se crea la clase TrabajadorDuplicadoExcepcion

---
+++ src/Excepciones/TrabajadorDuplicadoExcepcion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author AsusVivobook
+ */
+public class TrabajadorDuplicadoExcepcion extends RuntimeException {
+    public TrabajadorDuplicadoExcepcion(String mensaje) {
+        super(mensaje);
+    }
+}
+
