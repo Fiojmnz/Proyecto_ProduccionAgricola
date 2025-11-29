b81fa07eb22e292cf3167443d215445428ebc8c7
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-11-29 01:12:04 -0600

    Creacion
    Se crea la clase AlmacenamientoDTO

---
+++ src/Modelo/AlmacenamientoDTO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author AsusVivobook
+ */
+public class AlmacenamientoDTO {
+    private String producto;
+    private double cantidad;
+    private LocalDate fechaIngreso;
+    private LocalDate fechaEgreso;
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public LocalDate getFechaEgreso() {
+        return fechaEgreso;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public void setFechaEgreso(LocalDate fechaEgreso) {
+        this.fechaEgreso = fechaEgreso;
+    }
+}
