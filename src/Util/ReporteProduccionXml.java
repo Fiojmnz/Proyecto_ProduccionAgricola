5d6ba3c6e13bba6a500f2fe7466bb2eabbb7adc3
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-12-01 21:43:51 -0600

    Se modifica el Util
    ReporteProduccionXml

--- src/Util/ReporteProduccionXml.java
+++ src/Util/ReporteProduccionXml.java
@@ -11,21 +11,31 @@ import java.util.List;
  *
  * @author gipsy
  */
-public class ReporteProduccionXm {
-      public static String generarXML(List<ProduccionDTO> datos) {
+public class ReporteProduccionXml {
+    public static String generarXML(List<ProduccionDTO> datos) {
         StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
         sb.append("<reporteProduccion>\n");
         for (ProduccionDTO d : datos) {
             sb.append("  <cosecha>\n");
-            sb.append("    <CultivoId>").append(d.getCultivoId()).append("</cultivoId>\n");
-            sb.append("    <Fecha>").append(d.getFecha()).append("</fecha>\n");
-            sb.append("    <CantidadRecolectada>").append(d.getCantidadRecolectada()).append("</cantidad>\n");
-            sb.append("    <Calidad>").append(d.getCalidad()).append("</calidad>\n");
-            sb.append("    <Destino>").append(d.getDestino()).append("</destino>\n");
-            sb.append("    <Productividad>").append(d.getProductividad()).append("</productividad>\n");
-            sb.append("  </Cosecha>\n");
+            sb.append("    <id>").append(d.getId()).append("</id>\n");
+            sb.append("    <fecha>").append(esc(d.getFecha() != null ? d.getFecha().toString() : "")).append("</fecha>\n");
+            sb.append("    <cantidadRecolectada>").append(d.getCantidadRecolectada()).append("</cantidadRecolectada>\n");
+            sb.append("    <calidad>").append(esc(d.getCalidad())).append("</calidad>\n");
+            sb.append("    <destino>").append(esc(d.getDestino())).append("</destino>\n");
+            sb.append("    <productividad>").append(d.getProductividad()).append("</productividad>\n");
+            sb.append("  </cosecha>\n");
         }
         sb.append("</reporteProduccion>\n");
         return sb.toString();
+    }
+
+    private static String esc(String s) {
+        if (s == null) {
+            return "";
+        }
+        return s.replace("&", "&amp;")
+                .replace("<", "&lt;")
+                .replace(">", "&gt;");
     }
 }
