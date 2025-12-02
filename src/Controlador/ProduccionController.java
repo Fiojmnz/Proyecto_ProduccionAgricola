8305dfc3b6362ee687147b2a3a1bf3a3098bc285
fio_jmnz <fijimenezal@est.utn.ac.cr>
2025-12-01 21:40:45 -0600

    Se modifica ProduccionController

--- src/Controlador/ProduccionController.java
+++ src/Controlador/ProduccionController.java
@@ -3,36 +3,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controlador;
-
 import DAO.ProduccionDAO;
-import Enum.Destino;
-
 import Modelo.ProduccionDTO;
 import Servicios.ProduccionServicios;
-import java.sql.Date;
+
+import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.List;
+
+
 
 /**
  *
  * @author gipsy
  */
 public class ProduccionController {
-   private final ProduccionServicios service = new ProduccionServicios (new ProduccionDAO());
+    private final ProduccionServicios service;
 
-    public ProduccionDTO registrarProduccion(Integer cultivoId, Date fecha,double CantidadRecolectada, String calidad, Destino destino) {
+    // Constructor recibe la conexión y arma el servicio con el DAO
+    public ProduccionController(Connection conn) {
+        this.service = new ProduccionServicios(new ProduccionDAO(conn));
+    }
+
+    // Crear
+    public ProduccionDTO registrarProduccion(LocalDate fecha, double cantidad, String calidad, String destino) {
         ProduccionDTO dto = new ProduccionDTO();
-        dto.setCultivoId(cultivoId);
         dto.setFecha(fecha);
-        dto.setCantidadRecolectada(CantidadRecolectada);
+        dto.setCantidadRecolectada(cantidad);
         dto.setCalidad(calidad);
         dto.setDestino(destino);
         return service.registrar(dto);
     }
+ // Leer
+    public List<ProduccionDTO> listarProduccion() {
+        return service.listar();
+    }
 
-    public List<ProduccionDTO> listarProduccion(Date desde, Date hasta) {
-    LocalDate desdeLocal = desde.toLocalDate();
-    LocalDate hastaLocal = hasta.toLocalDate();
-    return service.listarPorRango(desdeLocal, hastaLocal);
-}
+    public List<ProduccionDTO> listarProduccionPorFecha(LocalDate inicio, LocalDate fin) {
+        return service.listarPorFecha(inicio, fin);
+    }
+
+    // Actualizar
+    public boolean actualizarProduccion(ProduccionDTO dto) {
+        return service.actualizar(dto);
+    }
+
+    // Eliminar
+    public boolean eliminarProduccion(int id) {
+        return service.eliminar(id);
+    }
+
+    // Reportes
+    public void generarReportePDF(String rutaArchivo) {
+        service.generarReportePDF(rutaArchivo);
+    }
+
+    public void generarReporteXML(String rutaArchivo) {
+        service.generarReporteXML(rutaArchivo);
+    }
 }
