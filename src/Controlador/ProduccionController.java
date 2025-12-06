d3adf1b06dfe26ab482abb589acf943bdd7b7b7d
Gipsy Aguirre L <giaguirrelo@est.utn.ac.cr>
2025-12-06 17:02:16 -0600

    Se modifica este controller Produccion

--- src/Controlador/ProduccionController.java
+++ src/Controlador/ProduccionController.java
@@ -10,7 +10,8 @@ import Servicios.ProduccionServicios;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.sql.Date;
+
 import java.util.List;
 
 
@@ -27,7 +28,7 @@ public class ProduccionController {
         Connection conn = ConnectionFactory.getInstancia().getConnection();
         this.service = new ProduccionServicios(new ProduccionDAO(conn));
     }
-    public ProduccionDTO registrarProduccion(LocalDate fecha, double cantidad, String calidad, String destino) {
+    public ProduccionDTO registrarProduccion(Date fecha, double cantidad, String calidad, String destino) {
         ProduccionDTO dto = new ProduccionDTO();
         dto.setFecha(fecha);
         dto.setCantidadRecolectada(cantidad);
@@ -40,9 +41,10 @@ public class ProduccionController {
         return service.listar();
     }
 
-    public List<ProduccionDTO> listarProduccionPorFecha(LocalDate inicio, LocalDate fin) {
-        return service.listarPorFecha(inicio, fin);
-    }
+public List<ProduccionDTO> listarProduccionPorFecha(Date inicio, Date fin) {
+    return service.listarPorFecha(inicio, fin);
+}
+
 
     public boolean actualizarProduccion(ProduccionDTO dto) {
         return service.actualizar(dto);
